e16b098b217b6117eb24ceed68f76cc3c3584777
Lee Briggs <lee@leebriggs.co.uk>
2018-01-04 11:15:21 -0800

    change-aws-credentials version v0.2.1

--- Formula/change-aws-credentials.rb
+++ Formula/change-aws-credentials.rb
@@ -1,9 +1,9 @@
 class ChangeAwsCredentials < Formula
   desc "A simple tool to change your AWS credentials quickly from the commandline"
   homepage "https://leebriggs.co.uk/"
-  url "https://github.com/jaxxstorm/change-aws-credentials/releases/download/v0.2.0/change-aws-credentials_0.2.0_darwin_amd64.tar.gz"
-  version "0.2.0"
-  sha256 "38563158674e6ee8d60028566f3b55bc66ed0d623fd660bf032bf0b88721096b"
+  url "https://github.com/jaxxstorm/change-aws-credentials/releases/download/v0.2.1/change-aws-credentials_0.2.1_darwin_amd64.tar.gz"
+  version "0.2.1"
+  sha256 "fcf93922675e7a670a8d4998cb89a6259757608daa1b84cf6939965a173acbe0"
 
   def install
     bin.install "change-aws-credentials"
